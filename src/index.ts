e189e2b40efb64495bf4f7f5c810a085d83169f6
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-01-14 13:34:11 +0100

    add simple route cache with manual adding

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
+import NodeCache from "node-cache";
 import { NextFunction, Request, Response } from "express";
 
 class RouteCache {
   str = "Hello, World!";
+  private _cache = new NodeCache();
+
   cache() {
-    return (req: Request, res: Response, next: NextFunction) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      const cacheKey = req.path;
+      const cachedData = await this._cache.get(cacheKey);
+
+      if (cachedData) return res.send(cachedData);
       next();
     };
+  }
+
+  addToCache(key: string, value: any, ttl: number) {
+    this._cache.set(key, value, ttl);
   }
 }
 
