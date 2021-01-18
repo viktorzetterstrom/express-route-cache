471eea3148d6f83ae0465eb23d909c4a96b2f74c
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-01-18 17:06:30 +0100

    wrap cache in own class in preparation for using different, and distributed caches

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import NodeCache from "node-cache";
 import { NextFunction, Request, Response } from "express";
 
+class Cache {
+  private cache = new NodeCache();
+
+  public async get<T>(key: string): Promise<T | undefined> {
+    return this.cache.get<T>(key);
+  }
+
+  public async set<T>(key: string, value: T, ttl: number) {
+    return this.cache.set<T>(key, value, ttl);
+  }
+}
+
 class RouteCache {
   str = "Hello, World!";
-  private _cache = new NodeCache();
+  private _cache = new Cache();
 
   cache() {
     return async (req: Request, res: Response, next: NextFunction) => {
