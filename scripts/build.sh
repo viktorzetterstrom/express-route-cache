5f1e10cd783cba60ec874be8d757b8d6d183fe30
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-04-14 08:20:16 +0200

    format build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rm -rf dist \
-&& ./node_modules/.bin/esbuild src/index.ts --format=esm --outdir=dist/esm --bundle --platform=node \
-&& ./node_modules/.bin/esbuild src/index.ts --format=cjs --outdir=dist/cjs --bundle --platform=node \
+&& ./node_modules/.bin/esbuild src/index.ts \
+  --format=esm --outdir=dist/esm --bundle --platform=node \
+&& ./node_modules/.bin/esbuild src/index.ts \
+  --format=cjs --outdir=dist/cjs --bundle --platform=node \
 && ./node_modules/.bin/tsc
